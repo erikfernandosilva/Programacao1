e4f94fd9a2aca33cc38e27697574c6b736f56bec
erikfernandosilva <52055811+erikfernandosilva@users.noreply.github.com>
2019-12-27 19:44:28 -0300

    Create Divisao.java

---
+++ Divisao.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author ferna
+ */
+// neste programa simples, é feita a divisão de dois numeros
+// pre estabelecidos. note que o calculo é feito diretamente na saida
+
+public class Divisao {
+    public static void main (String[] args){
+        int n1 = 6,n2 = 3;
+        System.out.println (n1/n2);
+    }
+}
